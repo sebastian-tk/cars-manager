71bbbcd7d1f08b46a33e398c94371fced9592436
Sebastian-jv <sebastian.tkaczykk@gmail.com>
2021-11-08 15:51:00 +0100

    refactor: Add TyreType in Wheel,change name SortByType to SortBy

--- persistence/src/main/java/tkaczyk/sebastian/persistence/Wheel.java
+++ persistence/src/main/java/tkaczyk/sebastian/persistence/Wheel.java
@@ -4,12 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import tkaczyk.sebastian.persistence.type.TyreType;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
 @Builder
 public class Wheel {
+    private TyreType type;
     private String model;
     private int size;
 }
---
+++ service/src/main/java/tkaczyk/sebastian/service/type/SortBy.java
@@ -0,0 +1,5 @@
+package tkaczyk.sebastian.service.type;
+
+public enum SortBy {
+    POWER_ENGINE,AMOUNT_COMPONENTS,SIZE_WHEEL
+}
